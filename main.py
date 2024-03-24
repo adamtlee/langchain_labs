b2ee9d4bd77062301c1f80a849e7c3760969b527
adamtlee <adam.lee346@gmail.com>
2024-03-24 11:10:05 -0600

    added gitignore

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
 from dotenv import load_dotenv
 
 load_dotenv() 
-def generate_pet_name():
+def generate_pet_name(animal_type):
     llm = OpenAI(temperature=0.7)
     
+    prompt_template_name = PromptTemplate(
+        input_variables=['animal_type'],
+        template="I have a pet {{animal_type}} and I want to name my pet. Suggest five names for my pet"
+    )
 
-    name = llm("I have a pet dog and I want to name my pet dog. Suggest five names for my pet dog")
+    name_chain = LLMChain(llm = llm, prompt = prompt_template_name)
 
-    return name 
+    response = name_chain({ 'animal_type': animal_type })
+
+    return response
 
 if __name__ == "__main__":
-    print(generate_pet_name())
+    print(generate_pet_name("dog"))
